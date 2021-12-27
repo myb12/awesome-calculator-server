b8761413f3e312b2d049377747f38d852bc63750
myb12 <mohammadyasinbappy@outlook.com>
2021-12-28 02:43:25 +0600

    main functionalities are done

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ const upload = require('./utilities/multer');
 const app = express();
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
 //======Database configuration======//
-const uri = "mongodb+srv://mydbuser2:EcHwYBhHvZWMhNgY@cluster0.ig1ef.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ig1ef.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const run = async () => {
     try {
         await client.connect();
         const database = client.db("calculatorDB");
-        const calculationCollection = database.collection("calculations");;
-        console.log('DB is connected');
+        const calculationCollection = database.collection("calculations");
 
         //======POST API for Add Calculations======//
         app.post('/addCalculation', upload.single('file'), async (req, res) => {
@@ -36,6 +34,18 @@ const run = async () => {
             const cursor = await calculationCollection.find({});
             const items = await cursor.toArray();
             res.send(items);
+        })
+
+        //======PUT API for Reordered Calculations======//
+        app.put('/update-results', async (req, res) => {
+            const data = req.body
+            const cursor = await calculationCollection.find({});
+            const results = await cursor.toArray();
+            let result;
+            const filter = { _id: results[0]._id };
+            const updateDoc = { $set: { reorderedResult: data } };
+            result = await calculationCollection.updateOne(filter, updateDoc);
+
         })
 
     } finally {
