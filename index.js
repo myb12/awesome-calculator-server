ac4f3c7d89c6e81271008cf54b888b32bdb44e04
myb12 <mohammadyasinbappy@outlook.com>
2021-12-27 15:18:19 +0600

    GET and POST api is created for calculations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const upload = require('./utilities/multer');
 const app = express();
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
@@ -8,11 +9,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//======Database configuration======//
+const uri = "mongodb+srv://mydbuser2:EcHwYBhHvZWMhNgY@cluster0.ig1ef.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const database = client.db("calculatorDB");
+        const calculationCollection = database.collection("calculations");;
+        console.log('DB is connected');
+
+        //======POST API for Add Calculations======//
+        app.post('/addCalculation', upload.single('file'), async (req, res) => {
+            try {
+                const data = { ...req.body, location: req.file.path, fileName: req.file.filename };
+                const result = await calculationCollection.insertOne(data);
+                res.json(result);
+            } catch (err) {
+                res.json(400);
+            }
+        });
+
+        //======GET API for Add Calculations======//
+        app.get('/results', async (req, res) => {
+            const cursor = await calculationCollection.find({});
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello Awesome Calculator');
 });
-
 
 app.listen(port, () => {
     console.log('Server is running on port', port);
---
+++ utilities/multer.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const path = require("path");
+const upload = multer({ dest: 'uploads/' })
+
+module.exports = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './uploads');
+        },
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + '-' + file.originalname);
+        }
+    }),
+
+    fileFilter: (req, file, cb) => {
+        let ext = path.extname(file.originalname);
+        if (ext !== ".txt") {
+            cb(new Error("Unsupported File Type!"), false);
+            return;
+        }
+        cb(null, true);
+    },
+});
